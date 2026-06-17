9fbb6ba8d6b92ae0fe99c6e734d6a22681ca4fd3
Aliaksei Makarevich <electron2686@gmail.com>
2026-06-17 20:47:20 +0300

    added logger logics to pkg

---
+++ pkg/logger/logger_config.go
@@ -0,0 +1,17 @@
+package logger
+
+// LoggerConfig - конфигурация логгера
+type LoggerConfig struct {
+	Level     string `yaml:"level" json:"level"`           // debug, info, warn, error
+	Format    string `yaml:"format" json:"format"`         // json, text
+	AddSource bool   `yaml:"add_source" json:"add_source"` // добавлять файл:строку
+}
+
+// DefaultLoggerConfig - дефолтная конфигурация логгера
+func DefaultLoggerConfig() *LoggerConfig {
+	return &LoggerConfig{
+		Level:     "info",
+		Format:    "json",
+		AddSource: true,
+	}
+}
